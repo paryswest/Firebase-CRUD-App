8156be0cb36537a5c069c01b9b35aa4512614444
Parys West <paryswest@gmail.com>
2021-11-21 16:10:48 -0500

    updated buttons, added increment for age in case a person turns a year + older

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import { db } from './firebase-config';
-import { collection, getDocs } from 'firebase/firestore';
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+} from 'firebase/firestore';
 
 function App() {
+  const [newName, setNewName] = useState('');
+  const [newAge, setNewAge] = useState(0);
   const [users, setUsers] = useState([]);
   const usersCollectionRef = collection(db, 'users');
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
+  };
+
+  const updateUser = async (id, age) => {
+    const userDoc = doc(db, 'users', id);
+    const newFields = { age: age + 1 };
+    await updateDoc(userDoc, newFields);
+  };
+
   useEffect(() => {
     const getUsers = async () => {
       const data = await getDocs(usersCollectionRef);
@@ -16,14 +35,32 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <input placeholder="Name" />
-      <input type="number" placeholder="Age" />
-      <button>Create user</button>
+      <input
+        placeholder="Name"
+        onChange={(event) => {
+          setNewName(event.target.value);
+        }}
+      />
+      <input
+        type="number"
+        placeholder="Age"
+        onChange={(event) => {
+          setNewAge(event.target.value);
+        }}
+      />
+      <button onClick={createUser}>Create user</button>
       {users.map((user) => {
         return (
           <div>
             <h1>Name: {user.name} </h1>
             <h1>Age: {user.age} </h1>
+            <button
+              onClick={() => {
+                updateUser(user.id, user.age);
+              }}
+            >
+              Increase Age{' '}
+            </button>
           </div>
         );
       })}
